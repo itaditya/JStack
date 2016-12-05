3f347090a1796960de98b03b3378881a746fb893
itaditya <adityaa803@gmail.com>
2016-12-05 20:16:54 +0530

    refining ng-cloak

--- public/views/auth/login/loginCtrl.js
+++ public/views/auth/login/loginCtrl.js
@@ -1,19 +1,24 @@
 angular.module('auth').controller('loginCtrl', function($scope,$location, authFactory,localStorageService, $routeParams,SessionService) {
   SessionService.setUserAuthenticated(false);
-  $scope.isLoading = false;
-  $scope.login = function(form) {
     $scope.isLoading = true;
-    if (form.$valid) {
-      console.log($scope.user);
-      authFactory.loginUser($scope.user).success(function(data) {
-        console.log(data);
-        data.type = "author";
-        // authorization.loadDashboard(data);
-        if(data.status === "1"){
-          SessionService.setUserAuthenticated(true);
-          $location.path("/profile");
-        }
-      });
+  $(document).ready(function(){
+    $scope.isLoading = false;
+    $scope.isValidating = false;
+    console.log($("input"));
+    $scope.login = function(form) {
+      $scope.isValidating = true;
+      if (form.$valid) {
+        console.log($scope.user);
+        authFactory.loginUser($scope.user).success(function(data) {
+          console.log(data);
+          data.type = "author";
+          // authorization.loadDashboard(data);
+          if(data.status === "1"){
+            SessionService.setUserAuthenticated(true);
+            $location.path("/profile");
+          }
+        });
+      }
     }
-  }
+  });
 });
