146aa20f9c6611ba1656718ae8d3ddc1af368766
itaditya <adityaa803@gmail.com>
2016-12-11 19:25:23 +0530

    heroku pushed

---
+++ app/models/category.js
@@ -0,0 +1,11 @@
+// // app/models/nerd.js
+// // grab the mongoose module
+// var mongoose = require('mongoose');
+
+// // define our nerd model
+// // module.exports allows us to pass this to other files when it is called
+// module.exports = mongoose.model('User', {
+//   frontend : {
+//     type: String
+//   }
+// });
